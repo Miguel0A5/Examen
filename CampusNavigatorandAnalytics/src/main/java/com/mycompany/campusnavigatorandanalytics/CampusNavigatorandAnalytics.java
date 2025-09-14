8894dd88a379286cdeafb670887ed1e624732a3f
MiguelA536 <2410307@alumnos.univo.edu.mx>
2025-09-13 18:38:55 -0600

    Creacíon del repositorio

---
+++ CampusNavigatorandAnalytics/src/main/java/com/mycompany/campusnavigatorandanalytics/CampusNavigatorandAnalytics.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.campusnavigatorandanalytics;
+
+/**
+ *
+ * @author migue
+ */
+public class CampusNavigatorandAnalytics {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
